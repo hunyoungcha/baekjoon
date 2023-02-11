4d3709d945993a2514b3db34520ac4d26abd70ad
hunyoungcha <chamgf5247@naver.com>
2023-02-11 23:16:10 +0900

    1

--- 1063.py
+++ 1063.py
@@ -1,46 +1,39 @@
 #65~72 : A~H
-#해결해볼것
-
-from pprint import pprint
-
-
-
-
+#해결
 k,s,n=map(str,input().split())
 n=int(n)
 
-board=[[0 for i in range(8)] for j in range(8)]
-board[int(k[1])-1][ord(k[0])-65]=1
-board[int(s[1])-1][ord(s[0])-65]=2
-board=board[::-1]
-pprint()
-    
+k=[ord(k[0]),int(k[1])]
+s=[ord(s[0]),int(s[1])]
 
-# for i in range(n):
-#     q=input()
-#     tmp_board=board
-#     k0=ord(k[0])
-#     k1=int(k[1])
+for i in range(n):
+    com=input()
+    tmp_k=k.copy() #그냥 다른 변수에 넣으면 메모리 주소를 그대로 복사하기 때문에 값도 같이 변경 됨, COPY명령어를 통해 다른 메모리에 저장
+    tmp_s=s.copy()
+    #커맨드 이동 명령
+    if 'R' in com:
+        k[0]+=1
+    elif 'L' in com:
+        k[0]-=1
+    if 'T' in com:
+        k[1]+=1
+    elif 'B' in com:
+        k[1]-=1
 
-#     if 'R' in q:
-#         k0+=1
-#         if k0>72:
-#             k0-=1
-#     elif 'L' in q:
-#         k0-=1
-#         if k0<65:
-#             k0+=1
-    
-#     if 'B' in q:
-#         k1-=1
-#         if k1<1:
-#             k1+=1
-#     elif 'T' in q:
-#         k1+=1    
-#         if k1>8:
-#             k1-=1
+    if k[0]==73 or k[0]==64 or k[1]==0 or k[1]==9:
+        k=tmp_k
+    if k==s:
+        if tmp_k[0]-k[0]==-1:
+            s[0]+=1
+        elif tmp_k[0]-k[0]==1:
+            s[0]-=1
+        if tmp_k[1]-k[1]==-1:
+            s[1]+=1
+        elif tmp_k[1]-k[1]==1:
+            s[1]-=1
+        if s[0]>72 or s[0]<65 or s[1]<1 or s[1]>8:
+            k=tmp_k
+            s=tmp_s
 
-
-#     k=chr(k0)+str(k1)
-
-
+print(chr(k[0])+str(k[1]))
+print(chr(s[0])+str(s[1]))

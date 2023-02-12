d056eb8e46f44c10d50e63cdef22cbd468af132e
hunyoungcha <chamgf5247@naver.com>
2023-02-12 23:49:11 +0900

    2

--- 1072.py
+++ 1072.py
@@ -1,19 +1,39 @@
 #승률 공식==y/x*100
 #해결 해볼것
-#브루트포스는 안될듯
+
+
+
+# 승률 구하는 방법
+# x,y=map(int,input().split())
+# cnt=-1
+
+
+# while x!=y:
+#     cnt+=1
+#     y+=1
+#     x+=1
+#     stop_p=(y/x*100+1.0)/100*x/y
+#     if 1>=stop_p:
+#         cnt+=1
+#         break
+
+# print(cnt)
+
+#패배률 구하는 방법
 x,y=map(int,input().split())
-x/=2
-y/=2
+xy=x-y
+half_x=x//100
+goal=(xy/x*100-1.0)/100
 cnt=0
-my=x-y
-p=round(my/x*100,1) #패배률 공식
-print(p)
-while True:
+if x==y:
+    cnt-=1
+elif xy/(x+half_x) >= goal:
+    x=half_x+x
+    cnt=half_x
+
+while x!=y:
+    if xy/x <= goal:
+        break
     cnt+=1
     x+=1
-    print(p-my/x*100)
-    print('cnt= ',cnt)
-    if p-my/x*100>=1.0:
-        break
-
 print(cnt)

d4caf64bc689e8282beb8ccf3a2eb40b55db51d6
hunyoungcha <chamgf5247@naver.com>
2023-02-21 13:08:44 +0900

    3

--- 1072.py
+++ 1072.py
@@ -19,21 +19,44 @@
 
 # print(cnt)
 
-#패배률 구하는 방법
 x,y=map(int,input().split())
-xy=x-y
-half_x=x//100
-goal=(xy/x*100-1.0)/100
-cnt=0
-if x==y:
-    cnt-=1
-elif xy/(x+half_x) >= goal:
-    x=half_x+x
-    cnt=half_x
+z=y*100//x
+start,end=0,1000000000
 
-while x!=y:
-    if xy/x <= goal:
-        break
-    cnt+=1
-    x+=1
-print(cnt)
+if z>=99:
+    print(-1)
+else:
+    while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# xy=x-y
+# half_x=x//100
+# goal=(xy/x*100-1.0)/100
+# cnt=0
+# if x==y:
+#     cnt-=1
+# elif xy/(x+half_x) >= goal:
+#     x=half_x+x
+#     cnt=half_x
+
+# while x!=y:
+#     if xy/x <= goal:
+#         break
+#     cnt+=1
+#     x+=1
+# print(cnt)
